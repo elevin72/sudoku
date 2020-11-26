7f2016f66acf22499f4928ae8cd629643529031e
elevin72 <elilevin11@gmail.com>
2020-11-26 17:44:24 +0200

    What

---
+++ tmp/modtest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (){
+    int p = 36;
+    int q = 9;
+    int index = 80;
+    std::cout << (index - (9 * (index / 9))) << std::endl;
+    /* std::cout << p  << " % "  << q << " = " << p%q << std::endl; */
+}
